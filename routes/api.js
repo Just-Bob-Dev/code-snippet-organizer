64e42e28220c1d27756b26dc875959911ac554eb
Robert Hutchins <rjhutchins22@gmail.com>
2017-07-21 16:34:38 -0400

    add controller functionality to clean up api and general(app) routes.

--- routes/api.js
+++ routes/api.js
@@ -1,56 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const Snippet = require('../models/snippet');
+const api = require('../controllers/apiControllers.js')
 
 
 
-router.get('/snippets', function(req, res){
-  Snippet.getActivity(function(err, snippet){
-    if(err){
-      console.log(err);
-    }
-    res.json(snippet);
-  })
-})
+router.get('/snippets', api.returnAllSnippets);
 
-router.post('/snippets', function(req, res){
-  var activity = req.body;
-  Snippet.createActivity(snippet, function(err, snippet){
-    if(err){
-      console.log(err);
-    }
-    res.json(snippet);
-  })
-})
+router.post('/snippets', api.postAndCreateSnip);
 
-router.get('/snippets/:snippetId', function(req, res){
-  let snipId = req.params.snippetId;
-  Snippet.findOne({_id: snipId}).then(function(snippet){
-    console.log(snippet);
-    res.send(snippet);
-  })
-})
+router.get('/snippets/:snippetId', api.findOneSnippetById);
 
-router.put('/snippets/:snippetId', function(req, res){
-  let snipId = req.params.snippetId;
-  let snippet = req.body;
-  Snippet.updateActivity(actId, snippet, {}, function(err, snippet){
-    if(err){
-      console.log("This is a PUT error:  " + err);
-    }
-    res.json(snippet);
-  })
-})
+router.put('/snippets/:snippetId', api.updateSnippetById);
 
-router.delete('/snippets/:snippetId', function(req, res){
-  let snipId = req.params.snippetId;
-  Snippet.deleteActivity(snipId, function(err){
-    if(err){
-      console.log("Deletion error: " + err);
-    }
-    res.send("you successfully deleted this activity.")
-  })
-})
+router.delete('/snippets/:snippetId', api.deleteSnippetById);
 
 // router.post('/snippets/:snippetId', passport.authenticate('basic', {session: false}),function(req, res){
 //   let actId = req.params.activityId;
--- routes/general.js
+++ routes/general.js
@@ -1,28 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const Snippet = require('../models/snippet');
+const app = require('../controllers/appControllers.js')
 
+//displays login page.
+router.get('/login', app.login);
 
-router.get('/login', function(req, res){
-  res.render('login');
-})
+//uses find to return all snips
+router.get('/home', app.getSnips);
 
-router.get('/home', function(req, res){
-  Snippet.find(function(err, snippet){
-    if(err){
-      console.log(err);
-    }
-    res.render('home', {snippetBlock: snippet});
-  })
-})
+//renders the newSnip creation page.
+router.get('/newSnip', app.newSnip);
 
+//Grabs the snippet selected by the user.
+router.get('/snip/:snipId', app.getOneSnippet);
 
-
-
-
-
-
-
+//Creates a new snip from the newSnip page.
+router.post('/newSnip', app.createSnippet);
 
 
 
